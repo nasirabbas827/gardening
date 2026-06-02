3f8a739179b883c7cfe216656717ddfe6a7ab142
NASIR ABBAS <85347981+nasirabbas827@users.noreply.github.com>
2026-06-02 11:24:08 +0500

    security: remove exposed credentials

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p2a2d-cpe9z7hr48z9vf4j($7k!88p-&pm-5meubjwwcdf*29e'
+SECRET_KEY = "YOUR_OWN_API_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
